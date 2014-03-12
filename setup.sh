5a23a106349697671f8817e107ae73f4f75a8258
Julián Vásquez <juvasquez88@gmail.com>
2014-03-12 09:49:23 -0500

    update using node v0.10.26

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.26
-nvm use v0.10.26
+nvm install v0.10.12
+nvm use v0.10.12
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
